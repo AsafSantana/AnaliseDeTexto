7120c1b510802851443b7eef2ede8edbaca76151
AsafSantana <asserbotelhosantana@hotmail.com>
2020-04-16 14:41:37 -0400

    Algumas funções do sklearn

---
+++ original.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 01:57:14 2019
+
+@author: Taimoor
+"""
+
+corpus = ['text text mining is interesting',
+          'text mining is the same as data mining',
+          'text and data mining have few differences']
+
+from sklearn.feature_extraction.text import CountVectorizer
+vec = CountVectorizer()
+X = vec.fit_transform(corpus)
+
+print(X.toarray()) ##aparecer um vetor das palavras do vetor
+
+print(vec.get_feature_names()) ## numero de palavras do vetor
+
+print(len(vec.get_feature_names())) ## numero da frequencia da palavra
+
+print(vec.vocabulary_) ## a palvra a a frequencia da mesma
